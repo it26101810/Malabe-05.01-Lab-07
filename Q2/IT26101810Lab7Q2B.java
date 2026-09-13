1ea09bb3b766d5ca3642e5a8e2b53daf905a5f54
it26101810 <it26101810@my.sliit.lk>
2026-09-13 08:33:05 +0530

    Lab7Q2B

---
+++ Q2/IT26101810Lab7Q2B.java
@@ -0,0 +1,16 @@
+public class IT26101810Lab7Q2B{
+public static void main(String args[]){
+	int i;
+	int j;
+	for(i=1;i<=5;i++)
+	{ 
+        System.out.print(i+" "+" "+"-");
+        for(j=1;j<=i;j++)
+		{
+			System.out.print(" * ");
+		}
+		System.out.println();
+	}
+}
+}
+		
