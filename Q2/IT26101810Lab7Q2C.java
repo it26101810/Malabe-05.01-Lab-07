71f5b454c21fa651cfc2fbe4b670a721ba740f61
it26101810 <it26101810@my.sliit.lk>
2026-09-13 08:34:03 +0530

    Lab7Q2C

---
+++ Q2/IT26101810Lab7Q2C.java
@@ -0,0 +1,15 @@
+public class IT26101810Lab7Q2C{
+public static void main(String args[]){
+	int i;
+	int j;
+	for(i=5;i>=1;i--)
+	{ 
+        for(j=1;j<=i;j++)
+		{
+			System.out.print(i);
+		}
+		System.out.println();
+	}
+}
+}
+		
